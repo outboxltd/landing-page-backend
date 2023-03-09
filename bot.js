8945d80a65d8330b666d37d260d4d95461c98b0e
Miri <mirifurman@gmail.com>
2023-03-09 14:35:07 +0200

    bot creates new LP

--- bot.js
+++ bot.js
@@ -1,63 +1,49 @@
-const { Bot,session  } = require("grammy");
+const { Bot, session, InlineKeyboard } = require("grammy");
 const axios = require(`axios`);
 const User = require("./models/userModel.js")
+const { upload, compressImage } = require('./upload.js')
+const { hydrateFiles } = require("@grammyjs/files");
+const path = require('path');
+const fs = require('fs');
 
 require("dotenv").config();
+const BASE_URL = process.env.NODE_ENV === 'production' ? 'https://landing-page-backend.onrender.com' : 'http://localhost:8000';
 
 
-const checkTelegramIdExist = async (telegramId) => {
-    const user = await User.findOne({ where: { telegramId: telegramId } });
-    return user;
-};
+const {
+    LPPairs,
+    checkPhoneExist,
+    updateUserTelegramId,
+    checkTelegramIdExist,
+    getUserLPsByUid,
+    sendMessage,
+    markupStartingOptions,
+    markupUserEvents,
+    setCurrentLP,
+    addNewLP,
+    DeleteLP
+} = require('./botUtills.js');
 
-const checkPhoneExist = async (phoneNumber) => {
-    const user = await User.findOne({ where: { phoneNumber }, });
-    return user;
-};
-const updateUserTelegramId = async (id, telegramId) => {
-
-    await User.update({ telegramId },{where: { id }});
-};
-
-
-const FromNumber = 'LandingPageBot';
-const Token = process.env.MESSAGE_TOKEN;
-
-const sendMessage = (ToNumber, MassageText) => {
-    let config = {
-        method: `get`,
-        url: `http://www.micropay.co.il/ExtApi/ScheduleSms.php?get=1&token=${Token}&msg=${MassageText}&list=${ToNumber}&from=${FromNumber}`,
-        headers: {}
-    };
-    axios(config)
-        .then((response) => {
-            console.log(JSON.stringify(response.data));
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-};
-
-
+const dest = path.join(__dirname, './uploads');
 
 const bot = new Bot(process.env.BOT_TOKEN);
 
-
+bot.api.config.use(hydrateFiles(process.env.BOT_TOKEN));
 
 bot.use(session({
     initial: () => ({
         user: null,
-        userEvents: null,
-        currentEvent: null,
+        userLandingPages: null,
+        currentLP: null,
         smsTokenFlag: {
             token: '',
             flag: false
         },
-        editEventFlag: {
+        editLPFlag: {
             flag: false,
             field: ''
         },
-        newEventFlag: {
+        newLPFlag: {
             flag: false,
             values: {},
             index: 0
@@ -76,29 +62,70 @@ bot.use(session({
     })
 }));
 
+//?start command
 bot.command("start", async (ctx) => {
     ctx.session.user = await checkTelegramIdExist(ctx.message.from.id);
     if (ctx.session.user) {
-    ctx.session.userEvents = await getUserEvents(ctx.session.user.hash);
-    if (ctx.session.userEvents.length === 1) {
-        ctx.session.currentEvent = ctx.session.userEvents[0];
-        markupStartingOptions(ctx, ctx.session.userEvents.length === 0);
+        ctx.session.userLandingPages = await getUserLPsByUid(ctx.session.user.uid);
+        if (ctx.session.userLandingPages.length === 1) {
+            ctx.session.currentLP = ctx.session.userLandingPages[0];
+            markupStartingOptions(ctx, ctx.session.userLandingPages.length === 0);
+            return;
+        }
+        markupUserEvents(ctx);
         return;
-    }
-    markupUserEvents(ctx);
-    return;
 
     }
     ctx.reply(`Hello, send your phone number to start!`);
 });
 
+
+
+
+//?when a button is clicked
+
+bot.on("callback_query:data", async (ctx) => {
+    const query = ctx.callbackQuery.data;
+
+    if (query === 'Create New Landing Page') {
+        ctx.session.newLPFlag.flag = true;
+        ctx.session.currentLP = null;
+        await ctx.reply('Creating New LP');
+        await ctx.reply(Object.values(LPPairs)[ctx.session.newLPFlag.index].alternative);
+    }
+
+    if (query === 'delete lp') {
+        DeleteLP(ctx)
+        await ctx.reply('Landing page deleted');
+        markupUserEvents(ctx);
+    }
+
+    if (!ctx.session.currentLP && await setCurrentLP(ctx, query)) {
+        markupStartingOptions(ctx);
+        return;
+    }
+
+
+    if (query === 'Cancel') {
+        ctx.session.currentLP = null;
+        markupUserEvents(ctx);
+        return;
+    }
+
+
+    await ctx.answerCallbackQuery();
+});
+
+
+//?when a message is sent
+
 bot.on("message", async (ctx) => {
     //* if received a phone number
     if (ctx.message.entities !== undefined && ctx.message.entities[0].type === "phone_number") {
         if (ctx.session.user) {
-            ctx.session.userEvents = await getUserEvents(ctx.session.user.hash);
+            ctx.session.userEvents = await getUserLPsByUid(ctx.session.user.uid);
             if (ctx.session.userEvents.length === 1) {
-                ctx.session.currentEvent = ctx.session.userEvents[0];
+                ctx.session.currentLP = ctx.session.userEvents[0];
                 markupStartingOptions(ctx);
                 return;
             }
@@ -132,10 +159,135 @@ bot.on("message", async (ctx) => {
         await ctx.reply('Verified Successfully!');
         ctx.session.smsTokenFlag.token = '';
         ctx.session.smsTokenFlag.flag = false;
-
-
+        markupUserEvents(ctx)
     }
-}
-);
+    if (ctx.session.newLPFlag.flag) {
+        let value = Object.values(LPPairs)[ctx.session.newLPFlag.index];
+        if(ctx.message.text==="null"){
+            ctx.session.newLPFlag.values[value.dbColumn] = null;
+        }
+        else{
+            ctx.session.newLPFlag.values[value.dbColumn] = ctx.message.text;
+        }
+
+        if(ctx.update.message.photo){
+            if(value.dbColumn==="hero"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/hero.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="image1"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/image1.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="image2"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/image2.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="image3"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/image3.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="testimonialImg1"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/testimonialImg1.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="testimonialImg2"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/testimonialImg2.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+            if(value.dbColumn==="testimonialImg3"){
+                const file = await ctx.getFile();
+                // Download the file to a temporary location.
+                const path = await file.download(`${dest}/testimonialImg3.webp`);
+                // Print the file path.
+                console.log("File saved at ", path);
+            }
+        }
+        if (Object.keys(ctx.session.newLPFlag.values).length === Object.keys(LPPairs).length) {
+            ctx.session.newLPFlag.values.uid = ctx.session.user.uid;
+            const addedLP = await addNewLP(ctx.session.newLPFlag.values);
+            const id=addedLP.id;
+            const filesToUpdate = ['hero', 'image1', 'image2', 'image3', 'testimonialImg1', 'testimonialImg2', 'testimonialImg3'];
+            for (const file of filesToUpdate) {
+                // const file = req.files[fieldName];
+                if (file) {
+                    const fileData = file[0];
+                    let prefix = '';
+                    if (file === 'hero') {
+                        prefix = 'hero';
+                    } else if (file.startsWith('image')) {
+                        prefix = `image${file.slice(-1)}`;
+                    } else {
+                        prefix = `testimonialImg${file.slice(-1)}`;
+                    }
+                    // const extension = fileData.originalname.split('.').pop();
+                    const oldFilePath = `./uploads/${prefix}.webp`;
+                    const newFilePath = `./uploads/${id}-${prefix}.webp`.replace(/\s+/g, '-');
+                    if (fs.existsSync(oldFilePath)) {
+                        fs.renameSync(oldFilePath, newFilePath);
+                    } else {
+                        console.log(`File ${oldFilePath} not found.`);
+                    }
+                }
+            }
+            const imagePath = (imageName) => `${BASE_URL}/uploads/${id}-${imageName}`;
+
+            updatedCompany = {
+                ...addedLP,
+                hero: imagePath('hero.webp'),
+                image1: imagePath('image1.webp'),
+                image2: imagePath('image2.webp'),
+                image3: imagePath('image3.webp'),
+                testimonialImg1: imagePath('testimonialImg1.webp'),
+                testimonialImg2: imagePath('testimonialImg2.webp'),
+                testimonialImg3: imagePath('testimonialImg3.webp')
+            };
+            await addedLP.update(updatedCompany);
+            ctx.session.newLPFlag.flag = false;
+            ctx.session.newLPFlag.values = {};
+            ctx.session.newLPFlag.index = 0;
+            if (addedLP) {
+                markupUserEvents(ctx, true, 'Company Added Successfully');
+                return;
+            }
+            ctx.reply('something went wrong');
+            return;
+        }
+        ctx.session.newLPFlag.index += 1;
+        value = Object.values(LPPairs)[ctx.session.newLPFlag.index];
+        ctx.reply(value.alternative);
+        return;
+    }
+
+    // bot.on(":photo", async (ctx) => {
+    //     // Prepare the file for download.
+    //     const file = await ctx.getFile();
+    //     // Download the file to a temporary location.
+    //     const url = file.getUrl();
+    //     const path = await file.download(`${dest}/file1.webp`);
+    //     // Print the file path.
+    //     console.log("File saved at ", path);
+    // })
+
+
+});
 
 bot.start();
---
+++ botUtills.js
@@ -0,0 +1,210 @@
+const { Bot, session, InlineKeyboard } = require("grammy");
+const axios = require(`axios`);
+const User = require("./models/userModel.js")
+const LandingPage = require("./models/companyModel.js")
+const { upload, compressImage } = require('./upload.js')
+
+const LPPairs = {
+    brand: { dbColumn: 'brand', alternative: 'company name' },
+    isRTL: { dbColumn: 'isRTL', alternative: 'is the landing page in hebrew (true/false)'},
+    // topButtonText: { dbColumn: 'topButtonText', alternative: 'top Button Text ( if you want this field to be empty write null)' },
+    // topButtonLink: { dbColumn: 'topButtonLink', alternative: 'top Button Link ( if you want this field to be empty write null)' },
+    // signupButtonText: { dbColumn: 'signupButtonText', alternative: 'signup Button Text' },
+    mainTitle: { dbColumn: 'mainTitle', alternative: 'main Title' },
+    subTitle: { dbColumn: 'subTitle', alternative: 'sub Title' },
+    isEmailAvailableInForm: { dbColumn: 'isEmailAvailableInForm', alternative: 'is Email Available In Form (true/false)'},
+    isNameAvailableInForm: { dbColumn: 'isNameAvailableInForm', alternative: 'is Name Available In Form (true/false)' },
+    isPhoneAvailableInForm: { dbColumn: 'isPhoneAvailableInForm', alternative: 'is Phone Available In Form (true/false)'  },
+    icon1: { dbColumn: 'icon1', alternative: 'icon 1 ( if you want this field to be empty write null)' },
+    middleTitle1: { dbColumn: 'middleTitle1', alternative: 'middle Title 1 ( if you want this field to be empty write null)' },
+    middleText1: { dbColumn: 'middleText1', alternative: 'middle Text 1 ( if you want this field to be empty write null)' },
+    icon2: { dbColumn: 'icon2', alternative: 'icon 2 ( if you want this field to be empty write null)' },
+    middleTitle2: { dbColumn: 'middleTitle2', alternative: 'middle Title 2 ( if you want this field to be empty write null)' },
+    middleText2: { dbColumn: 'middleText2', alternative: 'middle Text 2 ( if you want this field to be empty write null)'},
+    icon3: { dbColumn: 'icon3', alternative: 'icon 3 ( if you want this field to be empty write null)' },
+    middleTitle3: { dbColumn: 'middleTitle3', alternative: 'middle Title 3 ( if you want this field to be empty write null)' },
+    middleText3: { dbColumn: 'middleText3', alternative: 'middle Text 3 ( if you want this field to be empty write null)' },
+    title1: { dbColumn: 'title1', alternative: 'title 1 ( if you want this field to be empty write null)' },
+    description1: { dbColumn: 'description1', alternative: 'description1 ( if you want this field to be empty write null)' },
+    title2: { dbColumn: 'title2', alternative: ' title 2 ( if you want this field to be empty write null)' },
+    description2: { dbColumn: 'description2', alternative: 'description 2 ( if you want this field to be empty write null)' },
+    title3: { dbColumn: 'title3', alternative: 'title 3 ( if you want this field to be empty write null)'},
+    description3: { dbColumn: 'description3', alternative: 'description 3 ( if you want this field to be empty write null)' },
+    hero: { dbColumn: 'hero', alternative: 'hero image ( if you want this field to be empty write null)' },
+    heroAlt: { dbColumn: 'heroAlt', alternative: 'hero Alt ( if you want this field to be empty write null)' },
+    image1: { dbColumn: 'image1', alternative: 'image 1 ( if you want this field to be empty write null)' },
+    image1Alt: { dbColumn: 'image1Alt', alternative: 'image 1 Alt ( if you want this field to be empty write null)' },
+    image2: { dbColumn: 'image2', alternative: 'image 2 ( if you want this field to be empty write null)' },
+    image2Alt: { dbColumn: 'image2Alt', alternative: 'image 2 Alt ( if you want this field to be empty write null)' },
+    image3: { dbColumn: 'image3', alternative: 'image 3 ( if you want this field to be empty write null)'},
+    image3Alt: { dbColumn: 'image3Alt', alternative: 'image 3 Alt ( if you want this field to be empty write null)' },
+    testimonialName1: { dbColumn: 'testimonialName1', alternative: 'testimonial Name 1 ( if you want this field to be empty write null)' },
+    testimonialText1: { dbColumn: 'testimonialText1', alternative: 'testimonial Text 1 ( if you want this field to be empty write null)' },
+    testimonialImg1: { dbColumn: 'testimonialImg1', alternative: 'testimonial Img 1 ( if you want this field to be empty write null)' },
+    testimonialImg1Alt: { dbColumn: 'testimonialImg1Alt', alternative: 'testimonial Img 1 Alt ( if you want this field to be empty write null)' },
+    testimonialName2: { dbColumn: 'testimonialName2', alternative: 'testimonial Name 2 ( if you want this field to be empty write null)' },
+    testimonialText2: { dbColumn: 'testimonialText2', alternative: 'testimonial Text 2 ( if you want this field to be empty write null)' },
+    testimonialImg2: { dbColumn: 'testimonialImg2', alternative: 'testimonial Img 2 ( if you want this field to be empty write null)' },
+    testimonialImg2Alt: { dbColumn: 'testimonialImg2Alt', alternative: 'testimonial Img 2 Alt ( if you want this field to be empty write null)' },
+    testimonialName3: { dbColumn: 'testimonialName3', alternative: 'testimonial Name 3 ( if you want this field to be empty write null)'},
+    testimonialImg3: { dbColumn: 'testimonialImg3', alternative: 'testimonial Img 3 ( if you want this field to be empty write null)' },
+    testimonialText3: { dbColumn: 'testimonialText3', alternative: 'testimonial Text 3 ( if you want this field to be empty write null)' },
+    testimonialImg3Alt: { dbColumn: 'testimonialImg3Alt', alternative: 'testimonial Img 3 Alt ( if you want this field to be empty write null)' },
+    bottomFormHeader: { dbColumn: 'bottomFormHeader', alternative: 'bottom Form Header ( if you want this field to be empty write null)' },
+    aboutAddress: { dbColumn: 'aboutAddress', alternative: 'about Address ( if you want this field to be empty write null)' },
+    contactAddress: { dbColumn: 'contactAddress', alternative: 'contact Address ( if you want this field to be empty write null)' },
+    termsAddress: { dbColumn: 'termsAddress', alternative: 'terms Address ( if you want this field to be empty write null)' },
+    privacyAddress: { dbColumn: 'privacyAddress', alternative: 'privacy Address' },
+    facebookAddress: { dbColumn: 'facebookAddress', alternative: 'facebook Address ( if you want this field to be empty write null)' },
+    twitterAddress: { dbColumn: 'twitterAddress', alternative: 'twitter Address ( if you want this field to be empty write null)' },
+    instagramAddress: { dbColumn: 'instagramAddress', alternative: 'instagram Address ( if you want this field to be empty write null)' },
+    script: { dbColumn: 'script', alternative: 'Do you want to add script ( if you want this field to be empty write null) ' },
+    googleAnalyticsCode: { dbColumn: 'googleAnalyticsCode', alternative: 'Do you want to add script googleAnalyticsCode ( if you want this field to be empty write null)' },
+};
+
+const checkTelegramIdExist = async (telegramId) => {
+    const user = await User.findOne({ where: { telegramId: telegramId } });
+    return user;
+};
+
+const checkPhoneExist = async (phoneNumber) => {
+    const user = await User.findOne({ where: { phoneNumber }, });
+    return user;
+};
+const updateUserTelegramId = async (id, telegramId) => {
+
+    await User.update({ telegramId }, { where: { id } });
+};
+
+
+const getUserLPsByUid = async (uid) => {
+    const landingPages = await LandingPage.findAll({
+        where: { uid: uid },
+    });
+    return landingPages;
+};
+
+const FromNumber = 'LandingPageBot';
+const Token = process.env.MESSAGE_TOKEN;
+
+const sendMessage = (ToNumber, MassageText) => {
+    let config = {
+        method: `get`,
+        url: `http://www.micropay.co.il/ExtApi/ScheduleSms.php?get=1&token=${Token}&msg=${MassageText}&list=${ToNumber}&from=${FromNumber}`,
+        headers: {}
+    };
+    axios(config)
+        .then((response) => {
+            console.log(JSON.stringify(response.data));
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+};
+
+const markupStartingOptions = async (ctx, userHasNoEvents) => {
+    const inlineKeyboard = new InlineKeyboard();
+
+    if (userHasNoEvents) {
+        inlineKeyboard.text('New Landing Page').row();
+        ctx.reply("Hello, what you want to do ?", { reply_markup: inlineKeyboard });
+        return;
+    }
+
+    inlineKeyboard.text('Edit Current Landing Page', 'Edit Landing Page').row();
+    inlineKeyboard.text('Delete Current Landing page', 'delete lp').row();
+    inlineKeyboard.text('Choose A Different Landing Page or Create A New Landing Page', 'Cancel').row();
+    await ctx.reply(`Hello, what you want to do ? current event is ${ctx.session.currentLP.brand}`, { reply_markup: inlineKeyboard });
+};
+
+const markupUserEvents = async (ctx, isSecondTime, message) => {
+    const inlineKeyboard = new InlineKeyboard();
+    ctx.session.userLandingPages = await getUserLPsByUid(ctx.session.user.uid);
+    ctx.session.userLandingPages.forEach(lp => {
+        inlineKeyboard.text(lp.brand).row();
+    });
+    inlineKeyboard.text('Create New Landing Page').row();
+
+    if (isSecondTime) {
+        ctx.reply(message, { reply_markup: inlineKeyboard });
+    }
+    else {
+        ctx.reply("Choose a Landing Page to Start", { reply_markup: inlineKeyboard });
+    }
+    return;
+};
+
+// const markupUserEventFields = async (ctx, isAnotherUpdate, message) => {
+//     const inlineKeyboard = new InlineKeyboard();
+
+//     if (isAnotherUpdate) {
+//         ctx.session.currentEvent = await getUserEvent(ctx.session.currentEvent.hash);
+//     }
+
+//     Object.keys(ctx.session.currentEvent).forEach(key => {
+//         if (key === 'date') {
+//             inlineKeyboard.text(`${eventPairs[key].alternative} - ${ctx.session.currentEvent[key].toISOString().slice(0, 10)}`, eventPairs[key].dbColumn).row();
+//         }
+//         else if (key === 'time') {
+//             inlineKeyboard.text(`${eventPairs[key].alternative} - ${ctx.session.currentEvent[key].slice(0, 5)}`, eventPairs[key].dbColumn).row();
+//         }
+//         else if (eventPairs[key] !== undefined) {
+//             inlineKeyboard.text(`${eventPairs[key].alternative} - ${ctx.session.currentEvent[key]}`, eventPairs[key].dbColumn).row();
+//         }
+//     });
+//     inlineKeyboard.text('ביטול', 'Cancel Edit Event').row();
+
+//     if (isAnotherUpdate) {
+//         ctx.reply(message, { reply_markup: inlineKeyboard });
+//     }
+//     else {
+//         ctx.reply('what do you want to edit?', { reply_markup: inlineKeyboard });
+//     }
+// };
+
+const addNewLP = async (lp) => {
+    const createdCompany = await LandingPage.create(lp);
+    const user = await User.findOne({ where: { uid: lp.uid } });
+    console.log(user);
+    // const companyIds = user.companyIds.length()!==0? user.companyIds: [];
+    // if (!companyIds.includes(lp.id)) {
+    //     companyIds.push(lp.id);
+    // }
+    // await user.update({ companyIds });
+    return createdCompany;
+
+};
+
+const setCurrentLP = (ctx, query) => {
+    if (ctx.session.userLandingPages?.length === 1) {
+        ctx.session.currentLP = ctx.session.userLandingPages[0];
+        return ctx.session.currentLP;
+    }
+
+    ctx.session.userLandingPages?.forEach(lp => {
+        if (lp.brand === query) {
+            ctx.session.currentLP = lp;
+        }
+    });
+
+    return ctx.session.currentLP;
+};
+
+const DeleteLP=async (ctx)=>{
+    const landingPage = await LandingPage.findByPk(ctx.session.currentLP.id);
+    await landingPage.destroy();
+    return landingPage
+}
+module.exports = {
+    LPPairs,
+    checkPhoneExist,
+    updateUserTelegramId,
+    checkTelegramIdExist,
+    getUserLPsByUid,
+    sendMessage,
+    markupStartingOptions,
+    markupUserEvents,
+    setCurrentLP,
+    addNewLP,
+    DeleteLP
+};
